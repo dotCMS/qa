d1b5080caa6b001f770724f4e1cdaef2cc8badff
Brent Griffin <brent.griffin@dotcms.com>
2013-11-07 11:16:04 -0600

    Added BrowserStack functionality to POC and POC.properties file

--- src/main/java/com/dotcms/qa/selenium/util/SeleniumPageManager.java
+++ src/main/java/com/dotcms/qa/selenium/util/SeleniumPageManager.java
@@ -1,11 +1,14 @@
 package com.dotcms.qa.selenium.util;
 
+import java.net.URL;
 import java.util.concurrent.TimeUnit;
 
 import org.openqa.selenium.*;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.support.PageFactory;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
 
 import org.apache.log4j.Logger;
 
@@ -25,24 +28,60 @@ public class SeleniumPageManager{
     }
 
     private SeleniumPageManager(SeleniumConfig config) throws Exception {
-        String browserToTarget=config.getProperty("browserToTarget");
-        if("FIREFOX".equals(browserToTarget)) {
-            driver = new FirefoxDriver();
-        }
-        else if("CHROME".equals(browserToTarget)) {
-            throw new UnsupportedOperationException("ERROR - Chrome WebDriver not supported at this time.");
-        }
-        else if("SAFARI".equals(browserToTarget)) {
-            throw new UnsupportedOperationException("ERROR - Safari WebDriver not supported at this time.");
-        }
-        else if("ANDROID".equals(browserToTarget)) {
-            throw new UnsupportedOperationException("ERROR - Android WebDriver not supported at this time.");
-        }
-        else if("HTMLUNIT".equals(browserToTarget)) {
-            throw new UnsupportedOperationException("ERROR - HTMLUnit WebDriver not supported at this time.");
+        String useBrowserStack=config.getProperty("useBrowserStack");
+        logger.info("useBrowserStack=" + useBrowserStack);
+
+        if(useBrowserStack.toLowerCase().equals("true")) {
+            logger.info("Using BrowserStack");
+
+            String browserstackURL = config.getProperty("browserStack.URL");
+            logger.info("browserstackURL=" + browserstackURL);
+
+            DesiredCapabilities desiredCapabilities = new DesiredCapabilities();
+
+            String browserstackBrowser = config.getProperty("browserStack.capability.browser");
+            logger.info("browserstackBrowser=" + browserstackBrowser);
+            desiredCapabilities.setCapability("browser", browserstackBrowser);
+
+            String browserstackBrowserVersion = config.getProperty("browserStack.capability.browser.version");
+            logger.info("browserstackBrowserVersion=" + browserstackBrowserVersion);            
+            desiredCapabilities.setCapability("browser_version", browserstackBrowserVersion);
+
+            String browserstackOS = config.getProperty("browserStack.capability.browser.os");
+            logger.info("browserstackOS=" + browserstackOS);
+            desiredCapabilities.setCapability("os", browserstackOS);
+
+            String browserstackOSVersion = config.getProperty("browserStack.capability.os.version");
+            logger.info("browserstackOSVersion=" + browserstackOSVersion);
+            desiredCapabilities.setCapability("os_version", browserstackOSVersion);
+
+            String browserStackDebug = config.getProperty("browserStack.debug");
+            logger.info("browserStackDebug=" + browserStackDebug);
+            desiredCapabilities.setCapability("browserstack.debug", browserStackDebug);
+
+            driver = new RemoteWebDriver(new URL(browserstackURL), desiredCapabilities);
         }
         else {
-            throw new Exception("Config Error - unrecognized browserToTarget setting");
+            String browserToTarget=config.getProperty("browserToTarget");
+            logger.info("Targeting local " + browserToTarget + " browser.");
+            if("FIREFOX".equals(browserToTarget)) {
+                driver = new FirefoxDriver();
+            }
+            else if("CHROME".equals(browserToTarget)) {
+                throw new UnsupportedOperationException("ERROR - Chrome WebDriver not supported at this time.");
+            }
+            else if("SAFARI".equals(browserToTarget)) {
+                throw new UnsupportedOperationException("ERROR - Safari WebDriver not supported at this time.");
+            }
+            else if("ANDROID".equals(browserToTarget)) {
+                throw new UnsupportedOperationException("ERROR - Android WebDriver not supported at this time.");
+            }
+            else if("HTMLUNIT".equals(browserToTarget)) {
+                throw new UnsupportedOperationException("ERROR - HTMLUnit WebDriver not supported at this time.");
+            }
+            else {
+                throw new Exception("Config Error - unrecognized browserToTarget setting");
+            }
         }
 
         // Standardize settings for maximizing consistency
