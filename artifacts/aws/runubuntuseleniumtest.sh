690290e5602e3f8020b6769278101e01431051bf
Brent Griffin <brent.griffin@dotcms.com>
2015-02-23 11:37:32 -0600

    various scripting changes for automating 3.1 testing - also added some status checks throughout

--- artifacts/aws/runubuntuseleniumtest.sh
+++ artifacts/aws/runubuntuseleniumtest.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
 # Must be run as ubuntu user
 
+EXIT_CODE=255
+
 cd /home/ubuntu
 env
 
 export QA_TestStartTime=$(date +%Y%m%d_%H%M%S)
-export QA_StarterURL=s3://qa.dotcms.com/starters/3.0_qastarter_v.0.4b_release.zip
+export QA_StarterURL=s3://qa.dotcms.com/starters/3.1_qastarter_v.0.4b_c0ae3facdd.zip
 export QA_TomcatFolder=/opt/dotcms/dotserver/tomcat-7.0.54
 export QA_TomcatLogFile=${QA_TomcatFolder}/logs/catalina.out
 export QA_StarterFullFilePath=${QA_TomcatFolder}/webapps/ROOT/starter.zip
@@ -18,7 +20,7 @@ export QA_Country=US
 export QA_Language=en
 export QA_OS=Ubuntu
 export QA_Milestone=3.1
-export QA_RunLabel=${QA_Milestone}_${BUILD_TAG}_${QA_OS}_${QA_DB}_${QA_Browser}_${QA_Language}_${QA_Country}_${QA_TestStartTime}
+export QA_RunLabel=${QA_Milestone}_JenkinsSeleniumTester_${BUILD_NUMBER}_${QA_OS}_${QA_DB}_${QA_Browser}_${QA_Language}_${QA_Country}_${QA_TestStartTime}
 export QA_TestArtifactFilename=${QA_RunLabel}_Artifacts.tar.gz
 
 # export display to use Xvfb service
@@ -43,10 +45,16 @@ tar -xvf /home/ubuntu/downloads/dotcms.targz
 aws s3 cp ${QA_StarterURL} ${QA_StarterFullFilePath}
 #aws s3 cp s3://qa.dotcms.com/starters/3.0_qastarter_v.0.4b_release.zip ./dotserver/tomcat-7.0.54/webapps/ROOT/starter.zip
 
+# set index pages to legacy setting
+sed -i 's/CMS_INDEX_PAGE = index/CMS_INDEX_PAGE = index.html/g' ${QA_TomcatFolder}/webapps/ROOT/WEB-INF/classes/dotmarketing-config.properties
+
 # start dotCMS
 bin/startup.sh
+echo $?
 sleep 30
 logcount=`grep -c "org.apache.catalina.startup.Catalina start" ${QA_TomcatLogFile}`
+echo $?
+echo "logcount=${logcount}"
 while [ $logcount -lt 1 ]
 do
 	echo "sleeping..."
@@ -60,9 +68,13 @@ aws s3 cp s3://qa.dotcms.com/testautomation/dotcmsqa /home/ubuntu/.ssh/dotcmsqa
 chmod 600 /home/ubuntu/.ssh/dotcmsqa
 aws s3 cp s3://qa.dotcms.com/testautomation/dotcmsqa.pub /home/ubuntu/.ssh/dotcmsqa.pub
 chmod 600 /home/ubuntu/.ssh/dotcmsqa.pub
+
+echo "Host *">/home/ubuntu/.ssh/config
+echo "    StrictHostKeyChecking no">>/home/ubuntu/.ssh/config
+
 eval $(ssh-agent)
 ssh-add /home/ubuntu/.ssh/dotcmsqa
-ssh -Tq -o StrictHostKeyChecking=no git@github.com
+#ssh -Tq -o StrictHostKeyChecking=no git@github.com
 
 # clone qa repo
 cd /home/ubuntu
@@ -81,6 +93,8 @@ cd /home/ubuntu/qa
 pushd /home/ubuntu/qa/build/install/qa
 export JAVA_OPTS="-Dtestrail.Milestone=${QA_Milestone} -Dtestrail.RunLabel=${QA_RunLabel} -DbrowserToTarget=${QA_Browser} -Duser.language=${QA_Language} -Duser.country=${QA_Country}"
 bin/qa  -testjar lib/qa-0.1.jar -listener com.dotcms.qa.testng.listeners.TestRunCreator.class,com.dotcms.qa.testng.listeners.TestResultReporter.class -d "/home/ubuntu/testngresults_${QA_Database}_${QA_Browser}_${QA_Language}_${QA_Country}"
+EXIT_CODE=$?
+echo "EXIT_CODE=${EXIT_CODE}"
 #mv ./qa.log "${resultsdirectory}/testngresults_${database}_${browser}_${language}_${country}/qa.log"
 #mv ./*.png "${resultsdirectory}/testngresults_${database}_${browser}_${language}_${country}/."
 wait
@@ -103,8 +117,17 @@ rm -rf /home/ubuntu/temp_log/
 # store logs into s3
 aws s3 cp /home/ubuntu/${QA_TestArtifactFilename} s3://qa.dotcms.com/testartifacts/${QA_TestArtifactFilename}
 
+# copy testng results to workspace
+cp -a "/home/ubuntu/testngresults_${QA_Database}_${QA_Browser}_${QA_Language}_${QA_Country}" "${WORKSPACE}"
+ 
 # cleanup - prepare for another possible run
 sudo rm -rf /opt/dotcms 
 rm -rf /home/ubuntu/.aws
 rm -rf /home/ubuntu/downloads
 rm /home/ubuntu/${QA_TestArtifactFilename}
+
+echo "EXIT_CODE=${EXIT_CODE}"
+exit ${EXIT_CODE}
+
+#curl -u b.rent.griffin@dotcms.com:@s3cur3  https://raw.githubusercontent.com/dotCMS/qa/master/artifacts/aws/runubuntuseleniumtest.sh | bash -s
+
