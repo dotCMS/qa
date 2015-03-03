8075a672534a650faa4f0478d88c6c3fe1f5f425
Brent Griffin <brent.griffin@dotcms.com>
2015-03-03 14:02:24 -0600

    added qahost04 to qahost07 in preparation for host tests which require these additional hostnames to resolve.

--- artifacts/aws/configubuntuhostname.sh
+++ artifacts/aws/configubuntuhostname.sh
@@ -12,7 +12,7 @@ etchostname=`cat /etc/hostname`
 if [ `grep 127.0.0.1 /etc/hosts | grep -c "$etchostname"` -eq 0 ]
 then
     echo "Adding hostname to 127.0.0.1 in /etc/hosts"
-    sudo sed -i -r "s/^127.0.0.1([ \t].*)/127.0.0.1 $etchostname qademo.dotcms.com m.qademo.dotcms.com qashared.dotcms.com qahost01.dotcms.com qahost02.dotcms.com qahost03.dotcms.com\\1/g" /etc/hosts
+    sudo sed -i -r "s/^127.0.0.1([ \t].*)/127.0.0.1 $etchostname qademo.dotcms.com m.qademo.dotcms.com qashared.dotcms.com qahost01.dotcms.com qahost02.dotcms.com qahost03.dotcms.com qahost04.dotcms.com qahost05.dotcms.com qahost06.dotcms.com qahost07.dotcms.com\\1/g" /etc/hosts
 fi
 
 hostname=`hostname -f` || true
