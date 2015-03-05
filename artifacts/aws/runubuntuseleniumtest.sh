99d355dd2b777f08c051cda7c6144e83e8dc410f
Brent Griffin <brent.griffin@dotcms.com>
2015-03-05 08:05:35 -0600

    changed comment statements to echo commands to make debugging script in Jenkins easier

--- artifacts/aws/runubuntuseleniumtest.sh
+++ artifacts/aws/runubuntuseleniumtest.sh
@@ -23,17 +23,17 @@ export QA_Milestone=3.1
 export QA_RunLabel=${QA_Milestone}_JenkinsSeleniumTester_${BUILD_NUMBER}_${QA_OS}_${QA_DB}_${QA_Browser}_${QA_Language}_${QA_Country}_${QA_TestStartTime}
 export QA_TestArtifactFilename=${QA_RunLabel}_Artifacts.tar.gz
 
-# export display to use Xvfb service
+echo 'Exporting display to use Xvfb service'
 export DISPLAY=:99
 
-# create aws credentials
+echo 'Creating AWS credentials'
 mkdir /home/ubuntu/.aws
 echo '[default]' > /home/ubuntu/.aws/config
 echo 'region = us-east-1' >> /home/ubuntu/.aws/config
 echo 'aws_access_key_id = AKIAJB7GBDVDNTROV7LQ' >> /home/ubuntu/.aws/config
 echo 'aws_secret_access_key = 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' >> /home/ubuntu/.aws/config
 
-# pull down and extract dotCMS build
+echo 'Pulling down and extracting dotCMS build'
 mkdir /home/ubuntu/downloads
 wget -q -O ./downloads/dotcms.targz $QA_TAR_GZ_URL
 sudo mkdir -p /opt/dotcms
@@ -41,14 +41,14 @@ sudo chown -R ubuntu:ubuntu /opt/dotcms
 cd /opt/dotcms
 tar -xvf /home/ubuntu/downloads/dotcms.targz
 
-# pull down and replace starter
+echo 'Pulling down and replacing starter'
 aws s3 cp ${QA_StarterURL} ${QA_StarterFullFilePath}
 #aws s3 cp s3://qa.dotcms.com/starters/3.0_qastarter_v.0.4b_release.zip ./dotserver/tomcat-7.0.54/webapps/ROOT/starter.zip
 
-# set index pages to legacy setting
+echo 'Setting index pages to legacy setting'
 sed -i 's/CMS_INDEX_PAGE = index/CMS_INDEX_PAGE = index.html/g' ${QA_TomcatFolder}/webapps/ROOT/WEB-INF/classes/dotmarketing-config.properties
 
-# start dotCMS
+echo 'Starting dotCMS'
 bin/startup.sh
 echo $?
 sleep 30
@@ -63,7 +63,7 @@ do
 done
 echo "logcount=$logcount"
 
-# add ssh keys
+echo 'Adding ssh keys'
 aws s3 cp s3://qa.dotcms.com/testautomation/dotcmsqa /home/ubuntu/.ssh/dotcmsqa
 chmod 600 /home/ubuntu/.ssh/dotcmsqa
 aws s3 cp s3://qa.dotcms.com/testautomation/dotcmsqa.pub /home/ubuntu/.ssh/dotcmsqa.pub
@@ -76,22 +76,23 @@ eval $(ssh-agent)
 ssh-add /home/ubuntu/.ssh/dotcmsqa
 #ssh -Tq -o StrictHostKeyChecking=no git@github.com
 
-# clone qa repo
+echo 'Cloning qa repo'
 cd /home/ubuntu
 git clone git@github.com:dotCMS/qa.git
+echo "Checking out master-${DOTCMS_VERSION} branch"
 cd qa
 git checkout master-${DOTCMS_VERSION}
 
-# build and deploy qa plugin
+echo 'Building and deploying qa_automation plugin'
 cd /home/ubuntu/qa/plugins/com.dotcms.rest.qa_automation
 ./gradlew jar
 cp ./build/lib/com.dotcms.rest.qa_automation-0.1.jar ${QA_TomcatFolder}/webapps/ROOT/WEB-INF/felix/load/.
 
-# build selenium test
+echo 'Building testng/selenium tests'
 cd /home/ubuntu/qa
 ./gradlew installapp
 
-#	Run testng/selenium tests
+echo 'Running testng/selenium tests'
 pushd /home/ubuntu/qa/build/install/qa
 export JAVA_OPTS="-Dtestrail.Milestone=${QA_Milestone} -Dtestrail.RunLabel=${QA_RunLabel} -DbrowserToTarget=${QA_Browser} -Duser.language=${QA_Language} -Duser.country=${QA_Country}"
 bin/qa  -testjar lib/qa-0.1.jar -listener com.dotcms.qa.testng.listeners.TestRunCreator.class,com.dotcms.qa.testng.listeners.TestResultReporter.class -d "/home/ubuntu/testngresults_${QA_Database}_${QA_Browser}_${QA_Language}_${QA_Country}"
@@ -102,10 +103,10 @@ echo "EXIT_CODE=${EXIT_CODE}"
 wait
 popd
 
-#	Shutdown dotCMS
+echo 'Shutting down dotCMS'
 /opt/dotcms/bin/shutdown.sh
 
-# grab and package logs
+echo 'Grabbing and packaging logs'
 sleep 10
 mkdir /home/ubuntu/temp_log
 pushd /home/ubuntu/temp_log
@@ -116,13 +117,13 @@ tar -cvzf ../${QA_TestArtifactFilename} .
 popd
 rm -rf /home/ubuntu/temp_log/
 
-# store logs into s3
+echo 'Storing logs into s3'
 aws s3 cp /home/ubuntu/${QA_TestArtifactFilename} s3://qa.dotcms.com/testartifacts/${QA_TestArtifactFilename}
 
-# copy testng results to workspace
+echo 'Coping testng results to workspace'
 cp -a "/home/ubuntu/testngresults_${QA_Database}_${QA_Browser}_${QA_Language}_${QA_Country}" "${WORKSPACE}"
  
-# cleanup - prepare for another possible run
+echo 'Cleaning up - preparing for another possible run'
 sudo rm -rf /opt/dotcms 
 rm -rf /home/ubuntu/.aws
 rm -rf /home/ubuntu/downloads
