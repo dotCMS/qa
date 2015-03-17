edd126509b3a2579c3c17e2a989d37977e3ab44c
Brent Griffin <brent.griffin@dotcms.com>
2015-03-17 07:45:18 -0500

    Added code for splitting server and client tests - added s3 files to enable discovery of server from client

---
+++ artifacts/aws/SeleniumClient/initUbuntuHostnames.sh
@@ -0,0 +1,23 @@
+#!/bin/sh
+set -e
+
+etchostname=`cat /etc/hostname`
+#if [ `echo "$etchostname" | grep -c '\.'` -eq 0 ]
+#then
+#    echo "ERROR: /etc/hostname does not contain a fully qualified domain name."
+#    echo "edit /etc/hostname and try again"
+#    exit 1
+#fi
+
+if [ `grep 127.0.0.1 /etc/hosts | grep -c "$etchostname"` -eq 0 ]
+then
+    echo "Adding hostname to 127.0.0.1 in /etc/hosts"
+    sudo sed -i -r "s/^127.0.0.1([ \t].*)/127.0.0.1 $etchostname qademo.dotcms.com m.qademo.dotcms.com qashared.dotcms.com qahost01.dotcms.com qahost02.dotcms.com qahost03.dotcms.com qahost04.dotcms.com qahost05.dotcms.com qahost06.dotcms.com qahost07.dotcms.com\\1/g" /etc/hosts
+fi
+
+hostname=`hostname -f` || true
+if [ "$hostname" != "$etchostname" ]
+then
+    echo "Setting hostname to $etchostname"
+    sudo hostname "$etchostname"
+fi
--- artifacts/aws/jobs/nightlyrun.sh
+++ artifacts/aws/jobs/nightlyrun.sh
@@ -21,6 +21,8 @@ if [ "$LASTCOMMIT" != "$DOTCMS_COMMIT" ]; then
   echo "DOTCMS_ZIP_URL=${DOTCMS_ZIP_URL}" >> params
   echo "DOTCMS_TAR_GZ_URL=${DOTCMS_TAR_GZ_URL}" >> params
   echo "DOTCMS_COMMIT=${DOTCMS_COMMIT}" >> params
+  echo "QA_SERVER_STATUS_URL=s3://qa.dotcms.com/testautomation/commFiles/${BUILD_TAG}.status" >> params
+  echo "QA_SERVER_IP_URL=s3://qa.dotcms.com/testautomation/commFiles/${BUILD_TAG}.ip" >> params
 else
   if [ -f params ];
     then
