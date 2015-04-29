765a283550b47aa4426f7480f0ca1a4af098b001
Brent Griffin <brent.griffin@dotcms.com>
2015-04-29 09:11:51 -0500

    had to migrate testing to 3.2 build since 3.1 nightly has been deleted

--- artifacts/aws/jobs/nightlyRun.sh
+++ artifacts/aws/jobs/nightlyRun.sh
@@ -6,7 +6,7 @@ env
 
 export LASTCOMMIT=$(cat buildinfo.txt | python -c 'import sys, json; print json.load(sys.stdin)["contentlets"][0]["commitNumber"]')
 
-curl http://dotcms.com/api/content/query/+structureName:DotcmsNightlyBuilds%20+conHost:SYSTEM_HOST%20+DotcmsNightlyBuilds.version:3.1/orderby/moddate%20desc/limit/1 2>/dev/null > buildinfo.txt
+curl http://dotcms.com/api/content/query/+structureName:DotcmsNightlyBuilds%20+conHost:SYSTEM_HOST%20+DotcmsNightlyBuilds.version:3.2/orderby/moddate%20desc/limit/1 2>/dev/null > buildinfo.txt
 
 export DOTCMS_ZIP_URL=http://dotcms.com$(cat buildinfo.txt | python -c 'import sys, json; print json.load(sys.stdin)["contentlets"][0]["zip"]')
 
